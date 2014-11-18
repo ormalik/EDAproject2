0da03caded81357760e95e46e8c3e8beefdc1fae
ormalik <ormalik@aol.com>
2014-11-18 12:04:00 -0500

    Revert "EDA Project 2"
    This reverts commit 64664215ea043b28b2578fb21968fa97fe9c1781.

--- plot3.R.R
+++
@@ -1,8 +0,0 @@
-library(ggplot2)
-library(plyr)
-NEI <- readRDS("summarySCC_PM25.rds")
-baltimore <- subset(NEI, fips == "24510")
-head(baltimore)
-baltimore_type <- ddply(baltimore, .(year, type), function(x) sum(x$Emissions))
-colnames(baltimore_type)[3] <- "Emissions"
-qplot(year, Emissions, data=baltimore_type, color=type, geom="line")+ ggtitle("PM 2.5 Emissions in Baltimore by Type of Source")+xlab("Year")+ylab("Total PM 2.5 Emissions (Tons)")
--- plot4.R.R
+++
@@ -1,8 +0,0 @@
-SCC <- readRDS("Source_Classification_Code.rds")
-source_coal <- grep("coal", SCC$Short.Name, ignore.case = T)
-source_coal <- SCC[source_coal, ]
-NEI <- readRDS("summarySCC_PM25.rds")
-source_coal <- NEI[NEI$SCC %in% source_coal$SCC, ]
-source_coal_emissions <- aggregate(source_coal$Emissions, list(source_coal$year), FUN="sum")
-head(source_coal_emissions)
-plot(source_coal_emissions, type="l", xlab="Year", ylab="Total PM 2.5 Emissions from Coal (Tons)", main="Emissions of PM 2.5 from Coal Sources - 1999-2008")
